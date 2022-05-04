065a744279ec3ad418eb0b2ff7a4b1a55cefe08c
kmccorkle <kmccorkle9001@gmail.com>
2022-05-04 23:52:50 +0900

    Ignored Library folder.
    Added some files/folders.

---
+++ SteelLove/Assets/Scripts/ShipController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    [SerializeField]
+    float _thrustForce;
+
+    [SerializeField]
+    float _rotForce;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
