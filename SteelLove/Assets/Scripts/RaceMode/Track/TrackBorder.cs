7a399fd91277c248700d980b3b83ade5b8618393
nathanr08 <nream08@gmail.com>
2022-05-09 18:52:55 -0500

    added script for creating the track border
    sorry i moved some stuff around

---
+++ SteelLove/Assets/Scripts/RaceMode/Track/TrackBorder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TrackBorder : MonoBehaviour
+{
+    [SerializeField] private Transform _endPoint;
+    [SerializeField] private float _spriteDistance;
+
+    [SerializeField] private GameObject _borderObject;
+
+    private GameObject _lineObj;
+
+    private void Start()
+    {
+        _lineObj = new GameObject();
+        _lineObj.transform.parent = transform;
+        _lineObj.transform.localPosition = Vector3.zero;
+
+        float curDist = 0.0f;
+        float distToCover = (_endPoint.position - transform.position).magnitude;
+        Vector3 dirToEnd = (_endPoint.position - transform.position).normalized;
+        while(curDist < distToCover)
+        {
+            var spawnPos = dirToEnd * curDist;
+            var curBorderObj = Instantiate(_borderObject, _lineObj.transform);
+            curBorderObj.transform.localPosition = spawnPos;
+
+            curDist += _spriteDistance;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (Transform child in _lineObj.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        Destroy(_lineObj);
+    }
+}
