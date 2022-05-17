3b15ebb8b67699bf21a3fb9aae2196d56cc8e97a
Abopo <kmccorkle9001@gmail.com>
2022-05-17 22:56:35 +0900

    Added a label to doors when player gets close.

---
+++ SteelLove/Assets/Scripts/UI/DoorCloseTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCloseTrigger : MonoBehaviour {
+
+    [SerializeField] CanvasGroup _roomName;
+
+    // Start is called before the first frame update
+    void Start() {
+        _roomName.alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        _roomName.alpha = 1;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        _roomName.alpha = 0;
+    }
+}
