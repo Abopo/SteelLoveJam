c0b683892d185af4c2c5dd6be8095528dfb3843f
nathanr08 <nream08@gmail.com>
2022-05-21 11:22:59 -0500

    solved some collision issues
    fixed ship wall collision
    prevent ship from flying out of control when colliding off track

--- SteelLove/Assets/Scripts/RaceMode/AttachToTrack.cs
+++ SteelLove/Assets/Scripts/RaceMode/AttachToTrack.cs
@@ -11,6 +11,8 @@ public class AttachToTrack : MonoBehaviour
     [SerializeField] private float _normalSmoothSpeed;
     [SerializeField] private GameObject _centerPoint;
 
+    private Vector3 _latestNormal;
+
     private Rigidbody _rigidBody;
 
     private void Awake()
@@ -25,28 +27,34 @@ public class AttachToTrack : MonoBehaviour
 
     private void Attach()
     {
+        Vector3 hitPoint;
         RaycastHit hit;
         int mask = LayerMask.GetMask("TrackFloor");
         Ray rayForCast = new Ray(transform.position, -transform.up);
         if (Physics.Raycast(rayForCast, out hit, _raycastDist, mask))
         {
-            Quaternion fromTo = Quaternion.FromToRotation(_rigidBody.transform.up, hit.normal);
+            _latestNormal = hit.normal;
+        }
 
-            var newRot = Quaternion.Lerp(_rigidBody.rotation, fromTo * _rigidBody.rotation, Time.deltaTime * _normalSmoothSpeed);
-            var newVel = Vector3.Lerp(_rigidBody.velocity, fromTo * _rigidBody.velocity, Time.deltaTime * _normalSmoothSpeed);
+        Quaternion fromTo = Quaternion.FromToRotation(_rigidBody.transform.up, _latestNormal);
 
-            _rigidBody.MoveRotation(newRot);
-            _rigidBody.velocity = newVel;
+        var newRot = Quaternion.Lerp(_rigidBody.rotation, fromTo * _rigidBody.rotation, Time.deltaTime * _normalSmoothSpeed);
+        var newVel = Vector3.Lerp(_rigidBody.velocity, fromTo * _rigidBody.velocity, Time.deltaTime * _normalSmoothSpeed);
+
+        _rigidBody.MoveRotation(newRot);
+        _rigidBody.velocity = newVel;
+
+        if (hit.collider != null)
+        {
             _rigidBody.MovePosition(Vector3.Lerp(_rigidBody.position, hit.point + transform.up * _floatHight, Time.deltaTime * _hightSmoothSpeed));
+        }
 
-            // TODO: if track is flat then force the velocity to be flat
-            if (hit.normal == Vector3.up)
-            {
-                var flatVel = _rigidBody.velocity;
-                var mag = flatVel.magnitude;
-                flatVel.y = 0f;
-                _rigidBody.velocity = flatVel.normalized * mag;
-            }
+        if (_latestNormal == Vector3.up)
+        {
+            var flatVel = _rigidBody.velocity;
+            var mag = flatVel.magnitude;
+            flatVel.y = 0f;
+            _rigidBody.velocity = flatVel.normalized * mag;
         }
     }
 }
