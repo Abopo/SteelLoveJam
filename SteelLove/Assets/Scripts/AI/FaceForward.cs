4f7c23c27bf5c5bf0047716ef8dbea4d549ad8b5
kmccorkle <kmccorkle9001@gmail.com>
2022-05-26 21:59:50 +0900

    Updated FaceToward script so AI turn more naturally.

--- SteelLove/Assets/Scripts/AI/FaceForward.cs
+++ SteelLove/Assets/Scripts/AI/FaceForward.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FaceForward : MonoBehaviour {
 
     Rigidbody _rBody;
+    Vector3 _point;
+    Vector3 _toPoint;
+    [SerializeField] float speed;
 
     // Start is called before the first frame update
     void Start() {
@@ -15,7 +18,12 @@ public class FaceForward : MonoBehaviour {
     void Update() {
         if (_rBody.velocity.magnitude > 0) {
             // Rotate to face towards the rigidbody's velocity vector
-            transform.LookAt(transform.position + _rBody.velocity);
+            //transform.LookAt(transform.position + _rBody.velocity);
+
+            //_point = transform.position + _rBody.velocity;
+            //_toPoint = _point - transform.position;
+            Quaternion toRotation = Quaternion.LookRotation(_rBody.velocity);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, speed * Time.deltaTime);
         }
     }
 }
