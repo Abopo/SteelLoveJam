4a633aff187ed2174a3a3ca4dde9fd20c8b77171
nathanr08 <nream08@gmail.com>
2022-06-01 19:21:13 -0500

    added missed checkpoint UI
    fixed issue with engine on track 6

---
+++ SteelLove/Assets/Scripts/RaceMode/UI/CheckpointArrow.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointArrow : MonoBehaviour
+{
+    [SerializeField] private GameObject _arrowObj;
+    [SerializeField] private GameObject _missedCheckpointMessage;
+
+    [Header("Listening To")]
+    [SerializeField] private GameObjectEventChannelSO _onActiveCheckpointZoneLeft = default;
+    [SerializeField] private GameObjectEventChannelSO _onCrossedNextCheckpoint = default;
+
+    private bool _isDisplaying;
+    private GameObject _targetCheckpoint;
+
+    private void Start()
+    {
+        _arrowObj.SetActive(false);
+        _missedCheckpointMessage.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _onActiveCheckpointZoneLeft.OnEventRaised += CheckpointMissed;
+        _onCrossedNextCheckpoint.OnEventRaised += CrossedCheckpoint;
+    }
+
+    private void OnDisable()
+    {
+        _onActiveCheckpointZoneLeft.OnEventRaised -= CheckpointMissed;
+        _onCrossedNextCheckpoint.OnEventRaised -= CrossedCheckpoint;
+    }
+
+    private void Update()
+    {
+        if (_isDisplaying)
+        {
+            _arrowObj.transform.LookAt(_targetCheckpoint.transform);
+        }
+    }
+
+    private void CheckpointMissed(GameObject checkpoint)
+    {
+        _targetCheckpoint = checkpoint;
+        _isDisplaying = true;
+        _arrowObj.SetActive(true);
+        _missedCheckpointMessage.SetActive(true);
+    }
+
+    private void CrossedCheckpoint(GameObject checkpoint)
+    {
+        _targetCheckpoint = null;
+        _isDisplaying = false;
+        _arrowObj.SetActive(false);
+        _missedCheckpointMessage.SetActive(false);
+    }
+}
